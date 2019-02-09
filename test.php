d7ce9ba3390a73b86a5f5982dbb4097393724087
ccarupdas <arup.das@codeclouds.in>
2019-02-09 14:46:33 +0530

    base exclusion

--- test.php
+++ test.php
@@ -3,5 +3,6 @@ require_once(__DIR__.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.
 use Tdrive\App;
 $tdrive = new App;
 //print_r($tdrive->Init());
-//print_r($tdrive->CreateTheme('test'));
-print_r($tdrive->Deploy('46316060751'));
+print_r($tdrive->CreateTheme('test'));
+//test
+//print_r($tdrive->Deploy('46316060751'));
